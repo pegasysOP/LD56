12b8ce9aaccce923c373fd49de35a4279f019533
LGNDLeviathan <roitchie@gmail.com>
2024-10-04 20:28:45 +0100

    Create UI manager and methods for loading each scene

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+    public const string GAME_SCENE = "Test";
+    public const string MENU_SCENE = "Menu";
+    public const string CREDIT_SCENE = "Credits";
+    public const string INSTRUCTION_SCENE = "Instructions";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(GAME_SCENE);
+    }
+
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene(MENU_SCENE);
+    }
+
+    public void LoadCreditScene()
+    {
+        SceneManager.LoadScene(CREDIT_SCENE);
+    }
+
+    public void LoadInstructionScene()
+    {
+        SceneManager.LoadScene(INSTRUCTION_SCENE);
+    }
+
+    public void QuitButtonPressed()
+    {
+        Application.Quit();
+    }
+}
