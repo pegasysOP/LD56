ebcb3dc49476f9e0524a58a4528e0cb5116ee9c5
LGNDLeviathan <roitchie@gmail.com>
2024-10-05 09:54:51 +0100

    Add tile class

--- Assets/Scripts/Planning/BoardManager.cs
+++ Assets/Scripts/Planning/BoardManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Planning/Tile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    Vector2 tilePosition;
+    
+    public Tile(Vector2 tilePosition)
+    {
+        this.tilePosition = tilePosition;
+    }
+
+
+}
