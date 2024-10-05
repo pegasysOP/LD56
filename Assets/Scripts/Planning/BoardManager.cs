daeb5c77967ffc3e7400214856d6da08adade94c
LGNDLeviathan <roitchie@gmail.com>
2024-10-05 10:39:46 +0100

    Create methods to generate a board and place units on the board in Board Manager

--- Assets/Scripts/Planning/BoardManager.cs
+++ Assets/Scripts/Planning/BoardManager.cs
@@ -2,28 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Board : MonoBehaviour
+public class BoardManager : MonoBehaviour
 {
     Tile[,] board;
 
-    private int height = 8;
-    private int width = 8;
+    private readonly int height = 8;
+    private readonly int width = 8;
     // Start is called before the first frame update
     void Start()
     {
         board = new Tile[width,height];
-        for(int x=0; x < width; x++)
-        {
-            for( int y=0; y < height; y++)
-            {
-                board[x, y] = new Tile(new Vector2(x, y));
-            }
-        }
+        GenerateBoard();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void GenerateBoard()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                board[x, y] = new Tile(new Vector2(x, y));
+            }
+        }
+    }
+
+    void PlaceUnit(int x, int y)
+    {
+        Unit unit = new();
+        //TODO: Bounds checking
+        board[x,y].unit = unit;
+    }
+
+    public Tile[,] GetBoard()
+    {
+        return board;
     }
 }
