951905ae464bb675a100dee50830ffb7fcf43e80
LGNDLeviathan <roitchie@gmail.com>
2024-10-05 16:26:59 +0100

    Remove tilePosition field

--- Assets/Scripts/Planning/Tile.cs
+++ Assets/Scripts/Planning/Tile.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile
+public class Tile: MonoBehaviour
 {
-    //FIXME: We have a circular reference and tight coupling here. 
-    public Vector2 tilePosition;
     public Unit unit;
     
     public Tile(Vector2 tilePosition)
     {
-        this.tilePosition = tilePosition;
         unit = null; 
     }
-
-
 }
