13169e3603b4da374053eae24ada7d826ec28f42
LGNDLeviathan <roitchie@gmail.com>
2024-10-05 10:40:01 +0100

    Create helper methods for getting units and tiles

--- Assets/Scripts/Planning/BoardUtils.cs
+++ Assets/Scripts/Planning/BoardUtils.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class BoardUtils : MonoBehaviour
 {
+    BoardManager BM;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BM = FindObjectOfType<BoardManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static Unit GetUnitAtTile(int x, int y, BoardManager BM)
+    {
+        Tile[,] board = BM.GetBoard();
+
+        if (board[x,y].unit != null)
+        {
+            return board[x,y].unit;
+        }
+        return null; 
+    }
+
+    public static Tile GetTileAt(int x, int y, BoardManager BM)
+    {
+        GetNearestTile(x, y);
+        return null;
+    }
+
+    public static Tile GetNearestTile(int x, int y)
+    {
+        return null;
     }
 }
