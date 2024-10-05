c7568d22efd9c749af2887cb1c82f2e253163595
LGNDLeviathan <roitchie@gmail.com>
2024-10-05 09:59:08 +0100

    Create a grid of tiles

--- Assets/Scripts/Planning/BoardManager.cs
+++ Assets/Scripts/Planning/BoardManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    Tile[,] board;
 
+    private int height = 8;
+    private int width = 8;
     // Start is called before the first frame update
     void Start()
     {
-        
+        board = new Tile[width,height];
+        for(int x=0; x < width; x++)
+        {
+            for( int y=0; y < height; y++)
+            {
+                board[x, y] = new Tile(new Vector2(x, y));
+            }
+        }
     }
 
     // Update is called once per frame
