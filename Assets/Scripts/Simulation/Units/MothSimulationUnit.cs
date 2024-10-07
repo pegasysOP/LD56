e74955f501ef81dd3c2cea53af35dc2ddb3d551f
LGNDLeviathan <roitchie@gmail.com>
2024-10-07 17:46:53 +0100

    Implement Moth special attack (Moth perspective)

--- Assets/Scripts/Simulation/Units/MothSimulationUnit.cs
+++ Assets/Scripts/Simulation/Units/MothSimulationUnit.cs
@@ -15,7 +15,7 @@ public class MothSimulationUnit : SimulationUnitBase
         defence = 10;
         range = 2;
         attackTime = 4;
-        specialTime = 17;
+        specialTime = 1;
     }
 
     public override UnitType GetUnitType()
@@ -36,5 +36,33 @@ public class MothSimulationUnit : SimulationUnitBase
     {
         // DO CONFUSE DEBUFF AOE ABILITY HERE:
         // shoot projectile at target - cause confusion debuff to it and surrounding enemy units
+
+        //Check we have a valid target
+        if (!CanAttackCurrentTarget(ref currentGrid))
+        {
+            return;
+        }
+
+        Vector2Int targetPos = currentGrid.GetGridCoordinates(currentTarget);
+
+        //If so get all the units within the diagonals of that target
+
+        //Get all the opposing sides units on the board
+        Dictionary<SimulationUnitBase, int> units = currentGrid.GetUnitsInRange(currentGrid.GetGridCoordinates(currentTarget), 8, IsPlayerUnit(), !IsPlayerUnit());
+
+        //Now only set confused for those tht are on one of the four diagonal angles
+        foreach (SimulationUnitBase unit in units.Keys)
+        {
+            Vector2Int unitPos = currentGrid.GetGridCoordinates(unit);
+            int dx = Mathf.Abs(unitPos.x - targetPos.x);
+            int dy = Mathf.Abs(unitPos.y - targetPos.y);
+
+            //This is true for (2,2), (2,-2), (-2,2), (-2,-2) for example 
+            if(dx == dy)
+            {
+                unit.SetConfusionCounter(10);
+                //In BASE class: Set targeting and movement to be reversed and decrement confused timer
+            }
+        }
     }
 }
