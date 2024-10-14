4d353156587997d46038b037cce7c0fb0d8c4d63
LGNDLeviathan <roitchie@gmail.com>
2024-10-14 21:10:50 +0100

    Add inventory for player units

---
+++ Assets/Scripts/Planning/UnitInventory.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+
+public class UnitInventory
+{
+    public Dictionary<UnitType, int> Units { get; private set; }
+
+    public UnitInventory()
+    {
+        Units = new Dictionary<UnitType, int>
+        {
+            { UnitType.WorkerBee, 2 }, 
+            { UnitType.Beetle, 0 },
+            { UnitType.Moth, 0 },
+            { UnitType.Spider, 0 },
+            { UnitType.QueenBee, 0 }
+        };
+    }
+
+    public void AddUnit(UnitType unitType)
+    {
+        if (Units.ContainsKey(unitType))
+        {
+            Units[unitType]++;
+        }
+    }
+
+    public bool CanPlaceUnit(UnitType unitType)
+    {
+        return Units.ContainsKey(unitType) && Units[unitType] > 0;
+    }
+
+    public void PlaceUnit(UnitType unitType)
+    {
+        if (CanPlaceUnit(unitType))
+        {
+            Units[unitType]--;
+        }
+    }
+
+    public int GetUnitCount(UnitType unitType)
+    {
+        return Units.ContainsKey(unitType) ? Units[unitType] : 0;
+    }
+
+    public void DecrementUnit(UnitType unitType)
+    {
+        if (Units.ContainsKey(unitType) && Units[unitType] > 0)
+            Units[unitType]--;
+    }
+}
