aa1bfe5075962e6d332e2b15ed79556f3a9ff268
LGNDLeviathan <roitchie@gmail.com>
2024-10-16 21:53:02 +0100

    Implement inventory logic and prevent game starting till units are placed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private const int MaxLevels = 5; 
+    private const int MaxLevels = 5;
     private int level = 0;
 
     private Dictionary<Vector2Int, UnitType>[] levelEnemyStartStates;
     private List<UnitType[]> levelUpgradeTypes;
 
     public Sprite[] unitSprites;
-
     public static Dictionary<UnitType, Sprite> UnitTypeToSprite;
 
     UIManager UM;
@@ -20,6 +20,8 @@ public class GameManager : MonoBehaviour
     AudioManager AM;
 
     private UnitInventory inventory;
+    private GameObject currentDraggingUnit = null;
+    private bool isPlaying = false;
 
     void Start()
     {
@@ -27,9 +29,6 @@ public class GameManager : MonoBehaviour
         SetupSpriteMap();
         SetupUpgradeConfigurations();
         SetupEnemyStartStates();
-
-        BM.SpawnNewPlayerUnit(UnitType.WorkerBee);
-        BM.SpawnNewPlayerUnit(UnitType.WorkerBee);
         InitialiseInventory();
         BM.SavePlayerUnitStartPositions();
 
@@ -45,12 +44,65 @@ public class GameManager : MonoBehaviour
         inventory.AddUnit(UnitType.WorkerBee);
     }
 
+    void HandleUnitSelection()
+    {
+        if (currentDraggingUnit != null) return;
+        if (inventory.isInventoryEmpty()) return;
+        
+        if (Input.GetKeyDown(KeyCode.Alpha1) && inventory.CanPlaceUnit(UnitType.QueenBee))
+        {
+            BM.SpawnNewPlayerUnit(UnitType.QueenBee);
+            inventory.PlaceUnit(UnitType.QueenBee);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) && inventory.CanPlaceUnit(UnitType.Beetle))
+        {
+            BM.SpawnNewPlayerUnit(UnitType.Beetle);
+            inventory.PlaceUnit(UnitType.Beetle);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3) && inventory.CanPlaceUnit(UnitType.Spider))
+        {
+            BM.SpawnNewPlayerUnit(UnitType.Spider);
+            inventory.PlaceUnit(UnitType.Spider);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4) && inventory.CanPlaceUnit(UnitType.Moth))
+        {
+            BM.SpawnNewPlayerUnit(UnitType.Moth);
+            inventory.PlaceUnit(UnitType.Moth);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5) && inventory.CanPlaceUnit(UnitType.WorkerBee))
+        {
+            BM.SpawnNewPlayerUnit(UnitType.WorkerBee);
+            inventory.PlaceUnit(UnitType.WorkerBee);
+        }
+        CheckIfAllUnitsPlaced();  // Check after every unit placement
+    }
+
+    // New method to check if all units are placed
+    void CheckIfAllUnitsPlaced()
+    {
+        // Check if the inventory is empty (i.e. all units have been placed)
+        if (inventory.isInventoryEmpty() && !isPlaying)
+        {
+            UM.SetActiveStartButton(true);  // Show the start button
+            isPlaying = true;
+            BM.SavePlayerUnitStartPositions();
+        }
+    }
+
+    void Update()
+    {
+        HandleUnitSelection();
+    }
+
     void InitializeManagers()
     {
         UM = FindObjectOfType<UIManager>();
         BM = FindObjectOfType<BoardManager>();
         AM = FindObjectOfType<AudioManager>();
+
+        // Ensure the start button is hidden at the start of the game/level
         UM.SetActiveUpgradePanel(false);
+        UM.SetActiveStartButton(false);  // Start button is hidden initially
     }
 
     void SetupSpriteMap()
@@ -91,16 +143,16 @@ public class GameManager : MonoBehaviour
 
         switch (level)
         {
-            case 0: 
+            case 0:
                 enemyUnits.Add(new Vector2Int(7, 6), UnitType.WorkerBee);
                 break;
 
-            case 1: 
+            case 1:
                 enemyUnits.Add(new Vector2Int(6, 3), UnitType.WorkerBee);
                 enemyUnits.Add(new Vector2Int(7, 4), UnitType.Beetle);
                 break;
 
-            case 2: 
+            case 2:
                 enemyUnits.Add(new Vector2Int(6, 2), UnitType.QueenBee);
                 enemyUnits.Add(new Vector2Int(7, 2), UnitType.Spider);
                 enemyUnits.Add(new Vector2Int(7, 3), UnitType.Moth);
@@ -113,8 +165,8 @@ public class GameManager : MonoBehaviour
                 enemyUnits.Add(new Vector2Int(7, 4), UnitType.QueenBee);
                 break;
 
-            case 4: 
-                enemyUnits.Add(new Vector2Int(6, 3), UnitType.QueenBee); 
+            case 4:
+                enemyUnits.Add(new Vector2Int(6, 3), UnitType.QueenBee);
                 enemyUnits.Add(new Vector2Int(5, 3), UnitType.WorkerBee);
                 enemyUnits.Add(new Vector2Int(6, 2), UnitType.WorkerBee);
                 enemyUnits.Add(new Vector2Int(6, 4), UnitType.WorkerBee);
@@ -129,21 +181,24 @@ public class GameManager : MonoBehaviour
     {
         UM.upgradePanel.UnitChosen -= PickUpgradeUnit;
         inventory.AddUnit(unitType);
-        BM.SpawnNewPlayerUnit(unitType);
         AM.PlayUpgradeButtonClip();
         UM.SetActiveUpgradePanel(false);
-        UM.StartRoundButton.SetActive(true);
         BM.setSelectionEnabled(true);
+
+        // After upgrading a unit, wait for the player to place units before showing start button
+        isPlaying = false;
+        UM.SetActiveStartButton(false);  // Hide Start Button until units are placed
     }
 
     void LoadLevel()
     {
         SetUpgradeUnitsForLevel();
         PrepareBoardForNextLevel();
+
         if (level != 0)
             ShowUpgradePanel();
-        else
-            UM.StartRoundButton.SetActive(true);
+        //else
+            //UM.StartRoundButton.SetActive(true);  // First level, no upgrade, start immediately
     }
 
     void SetUpgradeUnitsForLevel()
@@ -156,16 +211,39 @@ public class GameManager : MonoBehaviour
     {
         AM.PlayPlanningPhaseClip();
         BM.ClearBoardUnits();
-        BM.LoadEnemyUnits(levelEnemyStartStates[level]);
-        BM.LoadPlayerUnits();
+        BM.LoadEnemyUnits(levelEnemyStartStates[level]);  // Load enemy units at start of level
+        BM.LoadPlayerUnits();  // Load player units
+        UM.SetActiveStartButton(false);  // Hide Start Button initially
     }
 
     void ShowUpgradePanel()
     {
         UM.SetActiveUpgradePanel(true);
         UM.upgradePanel.UnitChosen += PickUpgradeUnit;
-        UM.StartRoundButton.SetActive(false);
+        UM.SetActiveStartButton(false);  // Hide Start Button during upgrade panel
         BM.setSelectionEnabled(false);
+    }
+
+    public void StartLevel()
+    {
+        UM.StartRoundButton.SetActive(false);  // Hide Start Button once round begins
+        BM.SavePlayerUnitStartPositions();
+        BM.StartRound();
+        AM.PlayRegularButtonClip();
+        AM.PlaySimulationPhaseClip();
+    }
+
+    private void OnGameOver(object sender, bool playerWon)
+    {
+        OnRoundOver(playerWon);
+    }
+
+    void OnRoundOver(bool playerWon)
+    {
+        if (playerWon)
+            OnRoundWon();
+        else
+            OnRoundLost();
     }
 
     void OnRoundWon()
@@ -189,14 +267,6 @@ public class GameManager : MonoBehaviour
             LoadLevel();
     }
 
-    void OnRoundOver(bool playerWon)
-    {
-        if (playerWon)
-            OnRoundWon();
-        else
-            OnRoundLost();
-    }
-
     void GetNextLevel()
     {
         level++;
@@ -210,19 +280,5 @@ public class GameManager : MonoBehaviour
     {
         UM.StartRoundButton.SetActive(false);
         UM.LoadCreditScene();
-    }
-
-    public void StartLevel()
-    {
-        UM.StartRoundButton.SetActive(false);
-        BM.SavePlayerUnitStartPositions();
-        BM.StartRound();
-        AM.PlayRegularButtonClip();
-        AM.PlaySimulationPhaseClip();
-    }
-
-    private void OnGameOver(object sender, bool playerWon)
-    {
-        OnRoundOver(playerWon);
     }
 }
